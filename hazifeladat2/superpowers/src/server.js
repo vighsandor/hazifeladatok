c608050536ee4c0a40eb4064582880642eec4bba
Vigh Sandor <vigh.sandor@example.com>
2026-07-19 11:20:57 +0000

    9. LÉPÉS (Superpowers): Express server - kész a TDD megközelítésű implementáció

---
+++ hazifeladat2/superpowers/src/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const routes = require("./routes");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use("/api", routes);
+
+app.listen(PORT, () => {
+  console.log("Server fut a " + PORT + " porton");
+});
