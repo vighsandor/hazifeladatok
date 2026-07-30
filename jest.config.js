bc3a5c74c6d8bac391b6d978c8fbc601d0d71b8a
vighsandor <public.communication@vighsandor.hu>
2026-07-30 17:40:09 +0200

    chore: project setup - TypeScript, Jest, npm scripts

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/__tests__'],
+  testMatch: ['**/__tests__/**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  collectCoverage: false,
+};
